75dfdc3f7edc50db07da810b16085abfea68e1db
manish <manishrajput6196@gmail.com>
2023-08-20 18:52:17 +0530

    fix: blog page api base url

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,44 +1,40 @@
-import React from "react";
-import styles from "./page.module.css";
-import Image from "next/image";
-import { notFound } from "next/navigation";
+import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
+import { notFound } from 'next/navigation'
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(`/api/posts/${id}`, {
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
     return notFound()
   }
 
-  return res.json();
+  return res.json()
 }
 
-
 export async function generateMetadata({ params }) {
-
   const post = await getData(params.id)
   return {
     title: post.title,
     description: post.desc,
-  };
+  }
 }
 
 const BlogPost = async ({ params }) => {
-  const data = await getData(params.id);
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>
-            {data.desc}
-          </p>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
               src={data.img}
-              alt=""
+              alt=''
               width={40}
               height={40}
               className={styles.avatar}
@@ -47,21 +43,14 @@ const BlogPost = async ({ params }) => {
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src={data.img}
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
+          <Image src={data.img} alt='' fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-         {data.content}
-        </p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BlogPost;
+export default BlogPost
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,30 +1,34 @@
-import React from "react";
-import styles from "./page.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import React from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
+  const res = await fetch('/api/posts', {
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error('Failed to fetch data')
   }
 
-  return res.json();
+  return res.json()
 }
 
 const Blog = async () => {
-  const data = await getData();
+  const data = await getData()
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+      {data?.map(item => (
+        <Link
+          href={`/blog/${item._id}`}
+          className={styles.container}
+          key={item.id}
+        >
           <div className={styles.imageContainer}>
             <Image
               src={item.img}
-              alt=""
+              alt=''
               width={400}
               height={250}
               className={styles.image}
@@ -37,7 +41,7 @@ const Blog = async () => {
         </Link>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
